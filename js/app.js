35dde5be261af887533c73b400a17fc1ede39baf
Shokhruh555 <Shohruhismatov54@gmail.com>
2024-04-11 17:12:10 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+'use strict'
+
+
+//  FOR 1
+
+// for(let i = 0; i < 5; i++){
+//     console.log(1);
+// }
+
+// FOR 2
+
+// let a = 1 , b = 9;
+
+
+// for (let i = a; i < b+2; i++) {
+//     console.log(i);
+// }
+
+// FOR 3
+
+// let a = 1 , b= 10
+
+// for (let i = a+1; i < b; i++) {
+//     console.log(i);
+// }
+
+// FOR 4
+
+// let sum = 100;
+
+// for(let i = 1; i < 10; i++){
+//     console.log(sum * i);
+// }
+
+
+// FOR 5
+
+// let sum = 100
+// let o;
+// for(let i = 1; i < 10; i++){
+//     o = sum * i
+//     console.log(o / 10);
+// }
+
+
+// FOR 6
+
+
+// let sum = 100
+
+// let o;
+
+// for(let i = 1.2; i < 10+1; i++){
+//     o = sum * i
+//     console.log(o / 1);
+// }
+
+// FOR 7
+
+// let a= 4 , b= 5
+// let count = 0
+
+// for(let i = a; i <= b ; i++){
+//    count += i
+// }
+
+// console.log(count);
+
+
+// FOR 8
